1efc2452548319f76914e219d2ec3a4b653fdab6
BartekP81 <bartosz.pietras@gmail.com>
2024-11-08 12:14:18 +0100

    Quest1
    first commit after some time :)

---
+++ Quest1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string? army = Console.ReadLine();
+int result = 0;
+if(army != null && army.Length>0)
+{
+    foreach (char creature in army.ToCharArray())
+        if(creature=='B')
+            result+=1;
+        else if (creature=='C')
+            result+=3;
+}
+Console.WriteLine(result);
